5eb8a04833a4f45263f2314183321e75d53da08d
nebokali <nebokali@gmail.com>
2019-02-12 21:28:29 +0300

    Исправления

--- index.php
+++ index.php
@@ -15,37 +15,37 @@
     $items = [
         [
             'title' => '2014 Rossignol District Snowboard',
-            'category' => $categories[0],
+            'category' => 0,
             'price' => 10999,
             'url' => 'img/lot-1.jpg'
         ],
         [
             'title' => 'DC Ply Mens 2016/2017 Snowboard',
-            'category' => $categories[1],
+            'category' => 1,
             'price' => 159999,
             'url' => 'img/lot-2.jpg'
         ],
         [
             'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-            'category' => $categories[2],
+            'category' => 2,
             'price' => 8000,
             'url' => 'img/lot-3.jpg'
         ],
         [
             'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-            'category' => $categories[3],
+            'category' => 3,
             'price' => 10999,
             'url' => 'img/lot-4.jpg'
         ],
         [
             'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-            'category' => $categories[4],
+            'category' => 4,
             'price' => 7500,
             'url' => 'img/lot-5.jpg'
         ],
         [
             'title' => 'Маска Oakley Canopy',
-            'category' => $categories[5],
+            'category' => 5,
             'price' => 5400,
             'url' => 'img/lot-6.jpg'
         ]
@@ -123,7 +123,7 @@
                         <img src="<?=$value['url']; ?>" width="350" height="260" alt="<?=$item['title'];?>">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$value['category']; ?></span>
+                        <span class="lot__category"><?=$categories[$value['category']]; ?></span>
                         <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['title']; ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
