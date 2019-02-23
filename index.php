b280ba0171524ed9c6efd05f8e6cd49faf09a334
nebokali <nebokali@gmail.com>
2019-02-23 12:16:07 +0300

    Исправления

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
     require_once('boot.php');
 
-    print($layout_content);
-    
+    $page_content = include_template('index.php',[
+        'items' => $items, 
+        'categories' => $categories
+    ]);
+    $layout_content = include_template('layout.php', [
+        'content'    => $page_content,
+        'categories' => $categories,
+        'title'      => 'YetiCave - Главная страница'
+    ]);
+
+    print($layout_content); 
 ?>
